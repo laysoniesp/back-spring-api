677fd70dff1de94e729a0b6540b91f342e68c770
laysoniesp <Laysoniesp@gmail.com>
2026-09-22 14:48:34 -0300

    feat: add initial project structure with Maven and Spring Boot setup
    feat: adicionar estrutura inicial do projeto com configuração Maven e Spring Boot

---
+++ src/test/java/com/Aluno/Aluno/AlunoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Aluno.Aluno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlunoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
