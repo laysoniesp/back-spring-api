7b0c2ada165fbd6c73469e17c5acdfd912d794ff
laysoniesp <Laysoniesp@gmail.com>
2026-09-22 16:21:36 -0300

    feat: add interactive student creation in Main class
    feat: adicionar criação interativa de alunos na classe Main

--- src/main/java/com/aluno/Main.java
+++ src/main/java/com/aluno/Main.java
@@ -3,6 +3,8 @@ package com.aluno;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
+
+import java.util.Scanner;
 import com.aluno.service.AlunoService;
 import com.aluno.entity.Aluno;
 
@@ -15,11 +17,20 @@ public class Main implements CommandLineRunner {
         this.alunoService = alunoService;
     }
 
-
+    // Substituir pelo controller e endpoints REST
     @Override
     public void run(String... args) throws Exception {
         Aluno aluno = alunoService.criarAluno("João", 8.5);
         System.out.println("Aluno criado: " + aluno.getNome() + ", Nota: " + aluno.getNota());
         System.out.println("Aplicação iniciada com sucesso!");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o nome do aluno: ");
+        String nome = scanner.nextLine();
+        System.out.print("Digite a nota do aluno: ");
+        double nota = scanner.nextDouble();
+        Aluno novoAluno = alunoService.criarAluno(nome, nota);
+        System.out.println("Aluno criado: " + novoAluno.getNome() + ", Nota: " + novoAluno.getNota());
+
     }
 }
